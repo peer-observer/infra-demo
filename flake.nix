5f4598ce58f1d6a908706e3c481b33fa34c75978
0xb10c <b10c@b10c.me>
2025-12-15 18:36:41 +0100

    update: peer-observer-infra-library flake input
    • Removed input 'b10c-nix'
    • Removed input 'b10c-nix/nixpkgs'
    • Updated input 'peer-observer-infra-library':
        'github:0xB10C/peer-observer-infra-library/9f959080508791caf39d4531102598600ac5e9fe?narHash=sha256-IrFHrpq58cBQXOhmsLWncM/W0j1h7UZkVV5a6AQjo%2BY%3D' (2025-12-14)
      → 'github:peer-observer/infra-library/f09b95f3a328e06e60a23d75a8d205b85a098986?narHash=sha256-QGJNsw71FYWfZ4xB121rnWhLeJ0ad6hON77mZgMAKq8%3D' (2025-12-15)
    • Updated input 'peer-observer-infra-library/b10c-nix':
        follows 'b10c-nix'
      → 'github:0xb10c/nix/b803305e452d1b9d70cf183e4915ae5f456d6e39?narHash=sha256-6Y/bKPgxDgF/YVUK5NNODHNO%2BKHdfHUnQyY69NcBeds%3D' (2025-12-15)
    • Added input 'peer-observer-infra-library/b10c-nix/nixpkgs':
        follows 'peer-observer-infra-library/nixpkgs'

--- flake.nix
+++ flake.nix
@@ -3,14 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-    b10c-nix = {
-      url = "github:0xb10c/nix?ref=2025-12-peer-observer-protobuf-refactor";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     peer-observer-infra-library = {
-      url = "github:0xB10C/peer-observer-infra-library";
+      url = "github:peer-observer/infra-library";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.b10c-nix.follows = "b10c-nix";
     };
     disko = {
       url = "github:nix-community/disko";
@@ -23,7 +18,6 @@
       self,
       nixpkgs,
       peer-observer-infra-library,
-      b10c-nix,
       disko,
     }:
     let
